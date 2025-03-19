a4988903d3e208f0c513217f5254461f8858c48f
Tan Kulan <onurkulan1999@gmail.com>
2025-03-19 16:07:24 +0100

    [Rhi][Vk][Dx12] implementation changed accordigly with interface changes

--- Engine/Modules/Rhi/Source/Dx12/Dx12Device.cpp
+++ Engine/Modules/Rhi/Source/Dx12/Dx12Device.cpp
@@ -9,60 +9,72 @@ namespace Synergon::Rhi {
 	Dx12Device::~Dx12Device() {
 	}
 
-	std::unique_ptr<IDisplay> Dx12Device::createDisplay(const DisplayDescriptor& descriptor) {
-		throw std::logic_error("Not implemented");
+	std::unique_ptr<IDisplay> Dx12Device::createDisplay(const DisplayDescriptor &descriptor) {
+		throw std::logic_error("Not implemented yet");
 	}
 
-	std::shared_ptr<IBuffer> Dx12Device::createBuffer(const BufferDescriptor& descriptor) {
-		throw std::logic_error("Not implemented");
+	std::shared_ptr<IBuffer> Dx12Device::createBuffer(const BufferDescriptor &descriptor) {
+		throw std::logic_error("Not implemented yet");
 	}
 
-	std::shared_ptr<ITexture> Dx12Device::createTexture(const TextureDescriptor& descriptor) {
-		throw std::logic_error("Not implemented");
+	std::shared_ptr<ITexture> Dx12Device::createTexture(const TextureDescriptor &descriptor) {
+		throw std::logic_error("Not implemented yet");
 	}
 
-	std::shared_ptr<ISampler> Dx12Device::createSampler(const SamplerDescriptor& descriptor) {
-		throw std::logic_error("Not implemented");
+	std::shared_ptr<ISampler> Dx12Device::createSampler(const SamplerDescriptor &descriptor) {
+		throw std::logic_error("Not implemented yet");
 	}
 
-	std::shared_ptr<ITextureView> Dx12Device::createTextureView(const TextureViewDescriptor& descriptor) {
-		throw std::logic_error("Not implemented");
+	std::shared_ptr<IBufferView> Dx12Device::createBufferView(const BufferViewDescriptor &descriptor) {
+		throw std::logic_error("Not implemented yet");
 	}
 
-	std::unique_ptr<ICommandAllocator> Dx12Device::createCommandAllocator(const CommandAllocatorDescriptor& descriptor) {
-		throw std::logic_error("Not implemented");
+	std::shared_ptr<ITextureView> Dx12Device::createTextureView(const TextureViewDescriptor &descriptor) {
+		throw std::logic_error("Not implemented yet");
 	}
 
-	std::unique_ptr<ICommandQueue> Dx12Device::createCommandQueue(const CommandQueueDescriptor& descriptor) {
-		throw std::logic_error("Not implemented");
+	std::unique_ptr<ICommandAllocator> Dx12Device::createCommandAllocator(const CommandAllocatorDescriptor &descriptor) {
+		throw std::logic_error("Not implemented yet");
 	}
 
-	std::shared_ptr<IPipelineLayout> Dx12Device::createPipelineLayout(const PipelineLayoutDescriptor& descriptor) {
-		throw std::logic_error("Not implemented");
+	std::unique_ptr<ICommandQueue> Dx12Device::createCommandQueue(const CommandQueueDescriptor &descriptor) {
+		throw std::logic_error("Not implemented yet");
 	}
 
-	std::unique_ptr<IPipeline> Dx12Device::createRasterizerPipeline(const RasterizerPipelineDescriptor& descriptor) {
-		throw std::logic_error("Not implemented");
+	std::shared_ptr<IShader> Dx12Device::createShader(const ShaderDescriptor &descriptor) {
+		throw std::logic_error("Not implemented yet");
 	}
 
-	std::unique_ptr<IPipeline> Dx12Device::createComputePipeline(const ComputePipelineDescriptor& descriptor) {
-		throw std::logic_error("Not implemented");
+	std::shared_ptr<IShaderInputPool> Dx12Device::createShaderInputPool(const ShaderInputPoolDescriptor &descriptor) {
+		throw std::logic_error("Not implemented yet");
 	}
 
-	std::shared_ptr<IShader> Dx12Device::createShader(const ShaderDescriptor& descriptor) {
-		throw std::logic_error("Not implemented");
+	std::shared_ptr<IShaderInputLayout> Dx12Device::createShaderInputLayout(const ShaderResourceInputLayoutDescriptor &descriptor) {
+		throw std::logic_error("Not implemented yet");
 	}
 
-	std::shared_ptr<IShaderInputLayout> Dx12Device::createShaderInputLayout(const ShaderInputLayoutDescriptor& descriptor) {
-		throw std::logic_error("Not implemented");
+	std::shared_ptr<IShaderInputLayout> Dx12Device::createShaderInputLayout(const ShaderSamplerInputLayoutDescriptor &descriptor) {
+		throw std::logic_error("Not implemented yet");
 	}
 
-	std::shared_ptr<IShaderInputContainer> Dx12Device::createShaderInputContainer(const ShaderInputContainerDescriptor& descriptor) {
-		throw std::logic_error("Not implemented");
+	std::shared_ptr<IShaderInputContainer> Dx12Device::createShaderInputContainer(const ShaderInputContainerDescriptor &descriptor) {
+		throw std::logic_error("Not implemented yet");
 	}
 
-	std::unique_ptr<IFence> Dx12Device::createFence(const FenceDescriptor& descriptor) {
-		throw std::logic_error("Not implemented");
+	std::shared_ptr<IPipelineLayout> Dx12Device::createPipelineLayout(const PipelineLayoutDescriptor &descriptor) {
+		throw std::logic_error("Not implemented yet");
+	}
+
+	std::unique_ptr<IPipeline> Dx12Device::createComputePipeline(const ComputePipelineDescriptor &descriptor) {
+		throw std::logic_error("Not implemented yet");
+	}
+
+	std::unique_ptr<IPipeline> Dx12Device::createRasterizerPipeline(const RasterizerPipelineDescriptor &descriptor) {
+		throw std::logic_error("Not implemented yet");
+	}
+
+	std::unique_ptr<IFence> Dx12Device::createFence(const FenceDescriptor &descriptor) {
+		throw std::logic_error("Not implemented yet");
 	}
 
 }  // namespace Synergon::Rhi
--- Engine/Modules/Rhi/Source/Vk/VkDevice.hpp
+++ Engine/Modules/Rhi/Source/Vk/VkDevice.hpp
@@ -8,28 +8,30 @@ namespace Synergon::Rhi {
 	   public:
 		VkDevice();
 		~VkDevice() override;
+		std::unique_ptr<IDisplay>              createDisplay(const DisplayDescriptor &descriptor) override;
 
-		std::unique_ptr<IDisplay> createDisplay(const DisplayDescriptor& descriptor) override;
+		std::shared_ptr<IBuffer>               createBuffer(const BufferDescriptor &descriptor) override;
+		std::shared_ptr<ITexture>              createTexture(const TextureDescriptor &descriptor) override;
+		std::shared_ptr<ISampler>              createSampler(const SamplerDescriptor &descriptor) override;
 
-		std::shared_ptr<IBuffer>  createBuffer(const BufferDescriptor& descriptor) override;
-		std::shared_ptr<ITexture> createTexture(const TextureDescriptor& descriptor) override;
-		std::shared_ptr<ISampler> createSampler(const SamplerDescriptor& descriptor) override;
+		std::shared_ptr<IBufferView>           createBufferView(const BufferViewDescriptor &descriptor) override;
+		std::shared_ptr<ITextureView>          createTextureView(const TextureViewDescriptor &descriptor) override;
 
-		std::shared_ptr<ITextureView> createTextureView(const TextureViewDescriptor& descriptor) override;
+		std::unique_ptr<ICommandAllocator>     createCommandAllocator(const CommandAllocatorDescriptor &descriptor) override;
+		std::unique_ptr<ICommandQueue>         createCommandQueue(const CommandQueueDescriptor &descriptor) override;
 
-		std::unique_ptr<ICommandAllocator> createCommandAllocator(const CommandAllocatorDescriptor& descriptor) override;
-		std::unique_ptr<ICommandQueue>     createCommandQueue(const CommandQueueDescriptor& descriptor) override;
+		std::shared_ptr<IShader>               createShader(const ShaderDescriptor &descriptor) override;
 
-		std::shared_ptr<IPipelineLayout> createPipelineLayout(const PipelineLayoutDescriptor& descriptor) override;
-		std::unique_ptr<IPipeline>       createRasterizerPipeline(const RasterizerPipelineDescriptor& descriptor) override;
-		std::unique_ptr<IPipeline>       createComputePipeline(const ComputePipelineDescriptor& descriptor) override;
+		std::shared_ptr<IShaderInputPool>      createShaderInputPool(const ShaderInputPoolDescriptor &descriptor) override;
+		std::shared_ptr<IShaderInputLayout>    createShaderInputLayout(const ShaderResourceInputLayoutDescriptor &descriptor) override;
+		std::shared_ptr<IShaderInputLayout>    createShaderInputLayout(const ShaderSamplerInputLayoutDescriptor &descriptor) override;
+		std::shared_ptr<IShaderInputContainer> createShaderInputContainer(const ShaderInputContainerDescriptor &descriptor) override;
 
-		std::shared_ptr<IShader> createShader(const ShaderDescriptor& descriptor) override;
+		std::shared_ptr<IPipelineLayout>       createPipelineLayout(const PipelineLayoutDescriptor &descriptor) override;
+		std::unique_ptr<IPipeline>             createComputePipeline(const ComputePipelineDescriptor &descriptor) override;
+		std::unique_ptr<IPipeline>             createRasterizerPipeline(const RasterizerPipelineDescriptor &descriptor) override;
 
-		std::shared_ptr<IShaderInputLayout>    createShaderInputLayout(const ShaderInputLayoutDescriptor& descriptor) override;
-		std::shared_ptr<IShaderInputContainer> createShaderInputContainer(const ShaderInputContainerDescriptor& descriptor) override;
-
-		std::unique_ptr<IFence> createFence(const FenceDescriptor& descriptor) override;
+		std::unique_ptr<IFence>                createFence(const FenceDescriptor &descriptor) override;
 
 	   private:
 	};
