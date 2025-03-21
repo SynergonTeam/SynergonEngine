431e05c524e40874da9639578724532566f4401d
Tan Kulan <onurkulan1999@gmail.com>
2025-03-21 23:22:05 +0100

    [Rhi] Forgetten comment

--- Engine/Modules/Rhi/Include/Synergon/Rhi/IShaderInputContainer.hpp
+++ Engine/Modules/Rhi/Include/Synergon/Rhi/IShaderInputContainer.hpp
@@ -9,31 +9,6 @@
 
 #include <memory>
 
-/*
-	buffer1
-	buffer2
-	buffer3
-	texture1
-	texture2
-
-    DescriptorHeap [0 1 2 3 4 5]
-
-    buffer1View = DescriptorHeap[0]
-	buffer2View = DescriptorHeap[1]
-	buffer3View = DescriptorHeap[2]
-	texture1View = DescriptorHeap[3]
-	texture2View = DescriptorHeap[4]
-
-	VkDescriptorPool;
-
-	VkDescriptorSet1;
-	VkDescriptorSet2;
-
-	textureView != pool;
-
-
-*/
-
 namespace Synergon::Rhi {
 	class IShaderInputContainer {
 	   public:
