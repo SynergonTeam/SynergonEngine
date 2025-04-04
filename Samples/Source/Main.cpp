04c9101c80e65b9fdbe55f7caf3e8bb7597b8903
Zydak <koksszmoks@gmail.com>
2025-04-04 17:12:29 +0200

    [Window][Window Wrapper] pollEvents is now static

--- Samples/Source/Main.cpp
+++ Samples/Source/Main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char **argv) {
 		Synergon::Window::Window window(descriptor);
 
 		while (!window.shouldClose()) {
-			window.pollEvents();
+			Synergon::Window::Window::pollEvents();
 		}
 	}
 
