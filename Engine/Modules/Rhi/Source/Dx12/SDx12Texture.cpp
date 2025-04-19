9864e29d401244cd4e8b5a9391004f25570f7cc9
Arkadiy Pozharskiy <kiwi198@mail.ru>
2025-04-19 15:17:58 +0300

    WIP dx resources

---
+++ Engine/Modules/Rhi/Source/Dx12/SDx12Texture.cpp
@@ -0,0 +1,8 @@
+#include "SDx12Texture.hpp"
+
+namespace Synergon::Rhi {
+std::shared_ptr<ITextureView> SDx12Texture::createTextureView(const TextureViewDescriptor &descriptor) {
+	return nullptr;
+}
+
+}  // namespace Synergon::Rhi
